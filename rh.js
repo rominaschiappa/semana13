28bbb18f2387789b4ce6cd7864d14bde6447d9f4
rominaschiappa <romischiappa123@gmail.com>
2023-09-29 00:21:08 -0300

    html y js

--- rh.js
+++ rh.js
@@ -1,15 +1,33 @@
-let imagenesTemasEstudio = document.getElementById("imagenesTemasEstudio");
+document.addEventListener('DOMContentLoaded', () => {
+    const temaInput = document.getElementById('temaInput');
+    const listaTemas = document.getElementById('listaTemas');
 
-async function devolverImagen() {
-    try {
-        let response = await fetch("https://picsum.photos/");
-        let data = await response.json();
-        let imagenes = data.map((usuario) => usuario.name);
-        imagenesTemasEstudio.innerHTML = imagenes.map((nombre) => `<li>${nombre}</li>`).join("");
-    } catch (error) {
-        console.error("Error", error);
+    document.getElementById('agregarTema').addEventListener('click', () => {
+        const tema = temaInput.value.trim();
+        if (tema !== '') {
+            fetch('https://picsum.photos/200/200')
+                .then(response => response.url)
+                .then(imagenURL => {
+                    const listItem = createListItem(tema, imagenURL);
+                    listaTemas.appendChild(listItem);
+                    temaInput.value = '';
+                });
+        }
+    });
+
+    listaTemas.addEventListener('click', (event) => {
+        if (event.target.classList.contains('eliminar')) {
+            listaTemas.removeChild(event.target.parentElement);
+        }
+    });
+
+    function createListItem(tema, imagenURL) {
+        const listItem = document.createElement('li');
+        listItem.innerHTML = `
+            <img src="${imagenURL}" alt="${tema}">
+            <span>${tema}</span>
+            <button class="eliminar">Eliminar</button>
+        `;
+        return listItem;
     }
-}
-
-devolverImagen();
-
+});
